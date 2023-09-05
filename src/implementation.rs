e083511108843c31bb1006b4356a88b39db123c9
René Kijewski <rene.kijewski@fu-berlin.de>
2023-09-05 14:47:05 +0200

    Add `Display`, allow shadowed `core`, allow older versions

---
+++ src/implementation.rs
@@ -0,0 +1,100 @@
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+// All code in this file was extracted from:
+//  * https://github.com/dtolnay/anyhow/blob/0ba6408b5ef508c3dfc95797d21cfbdca9dd64ee/src/fmt.rs
+//  * https://github.com/dtolnay/anyhow/blob/fa9bcc0457a2e51593b874cc2f8bcb5608ad43fe/src/chain.rs
+//
+// Author: David Tolnay <dtolnay@gmail.com> and contributors to the `anyhow` project.
+// ////////////////////////////////////////////////////////////////////////////////////////////////
+
+use std::error::Error;
+use std::fmt::{self, Write};
+
+/// Write out the [`Error`] message and chain.
+///
+/// Please see the [`crate`] documentation for a more complete example.
+///
+/// ```rust
+/// use std::fmt::{self, Write};
+///
+/// pub enum MyError {
+///     Variant1(/* … */),
+///     Variant2(/* … */),
+///     // …
+/// }
+///
+/// impl fmt::Debug for MyError {
+///     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+/// #       /*
+///         pretty_error_debug::pretty_error_debug(self, f)
+/// #       */ Ok(())
+///     }
+/// }
+///
+/// // TODO: implement `std::fmt::Display` and `std::error::Error`.
+/// ```
+///
+/// # Errors
+///
+/// Fails if writing to the `f` argument failed.
+///
+pub fn pretty_error_debug(error: &dyn Error, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", error)?;
+    if let Some(cause) = error.source() {
+        write!(f, "\n\nCaused by:")?;
+        let multiple = cause.source().is_some();
+        for (n, error) in Chain(Some(cause)).enumerate() {
+            writeln!(f)?;
+            let mut indented = Indented {
+                inner: f,
+                number: if multiple { Some(n + 1) } else { None },
+                started: false,
+            };
+            write!(indented, "{}", error)?;
+        }
+    }
+    Ok(())
+}
+
+struct Chain<'a>(Option<&'a dyn Error>);
+
+impl<'a> Iterator for Chain<'a> {
+    type Item = &'a dyn Error;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let error = self.0?;
+        self.0 = error.source();
+        Some(error)
+    }
+}
+
+struct Indented<'a, 'b: 'a> {
+    inner: &'a mut fmt::Formatter<'b>,
+    number: Option<usize>,
+    started: bool,
+}
+
+impl<'a, 'b: 'a> fmt::Write for Indented<'a, 'b> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for (i, line) in s.split('\n').enumerate() {
+            if !self.started {
+                self.started = true;
+                match self.number {
+                    Some(number) => write!(self.inner, "{: >5}: ", number)?,
+                    None => self.inner.write_str("    ")?,
+                }
+            } else if i > 0 {
+                self.inner.write_char('\n')?;
+                if self.number.is_some() {
+                    self.inner.write_str("       ")?;
+                } else {
+                    self.inner.write_str("    ")?;
+                }
+            }
+
+            self.inner.write_str(line)?;
+        }
+
+        Ok(())
+    }
+}
